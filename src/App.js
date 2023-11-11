5217e98d04af41915f8f9c0d6f0212732b284e43
theo-thanhlam <phatlam.media.vis@gmail.com>
2023-11-11 02:59:20 -0500

    Finished: homepage layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <a href='/' className='logo'>MyBlog</a>
+        <nav>
+          <a href='/' className=''>Login</a>
+          <a href='/' className=''>Register</a>
+        </nav>
       </header>
-    </div>
+      
+      <div className='post'>
+        <div className='image'>
+          <img src='https://images.pexels.com/photos/1421903/pexels-photo-1421903.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='entry-img'></img>
+        </div>
+        <div className='texts'>
+        <h2>Ut est ipsum eu officia cupidatat esse duis.</h2>
+        <p>Ea mollit elit id proident sit minim nulla anim. Elit amet incididunt aliquip dolor sit dolor fugiat adipisicing. Cillum qui officia laboris sint in excepteur magna exercitation.</p>
+        </div>
+      </div>
+
+
+      <div className='post'>
+        <div className='image'>
+          <img src='https://images.pexels.com/photos/1421903/pexels-photo-1421903.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='entry-img'></img>
+        </div>
+        <div className='texts'>
+        <h2>Ut est ipsum eu officia cupidatat esse duis.</h2>
+        <p className='info'>
+          <a className='author' href='/'> Thanh Phat Lam</a>
+          <time>2023-11-11</time>
+        </p>
+        <p className='summary'>Ea mollit elit id proident sit minim nulla anim. Elit amet incididunt aliquip dolor sit dolor fugiat adipisicing. Cillum qui officia laboris sint in excepteur magna exercitation.</p>
+        </div>
+      </div>
+
+      <div className='post'>
+        <div className='image'>
+          <img src='https://images.pexels.com/photos/1421903/pexels-photo-1421903.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='entry-img'></img>
+        </div>
+        <div className='texts'>
+        <h2>Ut est ipsum eu officia cupidatat esse duis.</h2>
+        <p>Ea mollit elit id proident sit minim nulla anim. Elit amet incididunt aliquip dolor sit dolor fugiat adipisicing. Cillum qui officia laboris sint in excepteur magna exercitation.</p>
+        </div>
+      </div>
+
+
+      
+
+     
+    </main>
   );
 }
 

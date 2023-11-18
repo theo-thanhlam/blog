0de23713b20de0ccb591f10ac6721b7e5da47781
theo-thanhlam <phatlam.media.vis@gmail.com>
2023-11-18 02:08:58 -0500

    Finish: Registration process. Need error handling

--- src/pages/registerPage.js
+++ src/pages/registerPage.js
@@ -1,11 +1,49 @@
-import React from 'react'
+
+import React, { useState } from 'react'
 
 const RegisterPage = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function  register(event){
+    event.preventDefault();
+    const response = await fetch('http://localhost:3001/register', {
+      method:'POST',
+      body: JSON.stringify({email, password}),
+      headers:{'Content-Type': 'application/json'}
+    });
+
+    if(response.status === 200){
+      alert("Registration successful");
+    }else {
+      alert("Registration failed");
+    }
+  }
+
+
+
   return (
-    <form className='register'>
+    <form className='register' onSubmit={register}>
       <h1>Register</h1>
-        <input type="text" name="" id="" placeholder='Enter your username'/>
-        <input type="text" name="" id="" placeholder='Enter your password'/>
+      {/* Username */}
+        <input 
+        type="email" 
+        name="" 
+        id="" 
+        placeholder='Enter your email' 
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        />
+      {/* Password */}
+        <input 
+        type="password" 
+        name="" 
+        id="" 
+        placeholder='Enter your password'
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        />
         <button>Register</button>
     </form>
   )
